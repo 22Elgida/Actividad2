40aff848e2918977818035081cf79a30248a2880
22Elgida <elgida2502@gmail.com>
2022-08-08 20:10:20 -0600

    Actu

--- js/Funciones.js
+++ js/Funciones.js
@@ -1,6 +1,16 @@
 $(document).on('ready', Iniciar);
 
 function Iniciar(){
-    alert("Hola Gabriela ");
-    console.log("Te Amo Libni muaaaa");
+    alert("Gabriela");
+    console.log('Desarrollo Web')
+    $varBoton=$('#btnagregar');
+    $varBoton.on('click', Presionar);
+    
 }
+
+function Presionar(){
+    alert("Presionaste el boton");
+    $varTabla=$('#tabla');
+    $varTabla.hide();
+}
+
