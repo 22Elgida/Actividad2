92b86376708a2e79500d1ce98204be165f41e11a
22Elgida <elgida2502@gmail.com>
2022-08-08 22:59:27 -0600

    JS

--- js/Funciones.js
+++ js/Funciones.js
@@ -2,26 +2,40 @@ $(document).on('ready', Iniciar);
 
 function Iniciar(){
     alert("Gabriela");
-    console.log('Desarrollo Web')
+    console.log("Desarrollo Web");
     $varBoton=$('#btnagregar');
     $varBoton.on('click', Presionar);
+    $varBotonL=$('#btnlistar');
+    $varBotonL.on('click', Listar);
+    $varBotonG=$('#btnguardar');
+   $varBotonG.on('click', Guardar);
+   $varBotonG.hide();
     
 }
 
 function Presionar(){
-    alert("Presionaste el boton");
+    alert("Presionaste el boton Agregar");
     $varTabla=$('#tabla');
     $varTabla.hide();
-
-   
-}
-
-function Formu(){
+    $varBotonL.hide();
+    $varBotonG.show();
     $varFormulario=$('#Formulario');
-    $varFormulario.append('<input type="text" name="D" placeholder="Datos">')
+    $varFormulario.append('<input type="text" name="D" placeholder="NOMBRE">')
+    $varFormulario.append('<input type="text" name="D" placeholder="APELLIDO">')
+    $varFormulario.append('<input type="text" name="D" placeholder="CARNET">')
+    $varFormulario.append('<input type="text" name="D" placeholder="CURSOS">')
     console.log("Formulario");
-    $varFormulario=S('#Formulario'); 
+    $varFormulario=$('#Formulario'); 
 }
 
+function Listar(){
+    alert("Presionaste el boton Listar");
+    $varListar=$('#Listar');
+}
 
-
+function Guardar(){
+    alert("Presionaste el boton Guardar");
+    $varBotonG=$('#Guardar');
+    $varFormulario.hide();
+    $varTabla.show();
+}
